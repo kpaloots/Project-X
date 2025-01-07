0741561aa763b4cd74c0ae87129e89ff4357c874
kpaloots <kristjan.paloots1@gmail.com>
2025-01-07 07:43:45 +0200

    Commit 1.1

--- frontend/vue-project/src/components/GoalsOverview.vue
+++ frontend/vue-project/src/components/GoalsOverview.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+    <!-- Goals Overview kast -->
     <div class="goals-container">
       <div class="goals-overview">
         <section>
@@ -14,42 +16,41 @@
           </div>
         </section>
       </div>
-  
-      <!-- Lisame +New Goal nupu -->
-      <button class="new-goal-button">+ New Goal</button>
-  
-      <!-- Lisame väiksema sisestuskasti -->
-      <div class="promise-container">
-        <input
-          type="text"
-          class="promise-input"
-          placeholder="Today I promise to..."
-        />
-        <button class="send-button">
-          <img src="@/assets/icons/send-icon.png" alt="Send" />
-        </button>
-      </div>
     </div>
-  </template>
-  
-  
-  <script>
-  import GoalCard from './GoalCard.vue'
-  
-  export default {
-    name: 'GoalsOverview',
-    components: {
-      GoalCard,
-    },
-    data() {
-      return {
-        activeGoals: [
-          { id: 1, title: 'Goal 1', progress: 72 },
-          { id: 2, title: 'Goal 2', progress: 50 },
-        ],
-        completedGoals: [{ id: 3, title: 'Goal 3', progress: 100 }],
-      }
-    },
-  }
-  </script>
-  
+
+    <!-- +New Goal nupp -->
+    <button class="new-goal-button">+ New Goal</button>
+
+    <!-- "Today I promise to..." kast -->
+    <div class="promise-container">
+      <input
+        type="text"
+        class="promise-input"
+        placeholder="Today I promise to..."
+      />
+      <button class="send-button">
+        <img src="@/assets/icons/send-icon.png" alt="Send" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import GoalCard from './GoalCard.vue'
+
+export default {
+  name: 'GoalsOverview',
+  components: {
+    GoalCard,
+  },
+  data() {
+    return {
+      activeGoals: [
+        { id: 1, title: 'Goal 1', progress: 72 },
+        { id: 2, title: 'Goal 2', progress: 50 },
+      ],
+      completedGoals: [{ id: 3, title: 'Goal 3', progress: 100 }],
+    }
+  },
+}
+</script>
